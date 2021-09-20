0e3ca0a92c725d81c7db1c27cce31ba0c6674851
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-09-20 13:37:42 +0900

    bool値について学習。

---
+++ boolean.rb
@@ -0,0 +1,17 @@
+puts true.class
+puts false.class
+# trueもfalseもobjectの一部らしい。
+
+# 比較演算子
+puts 1 == 1
+puts 1 == 2
+puts 1 != 2
+
+# 反転
+true_val = true
+false_val = false
+puts !true_val == false_val
+
+# nilの判別
+val = nil
+puts val.nil?
