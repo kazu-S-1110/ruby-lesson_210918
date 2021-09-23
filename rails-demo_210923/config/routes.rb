bde95bcfff0083939b4e07f026a55a39cb4da6a8
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-09-23 19:39:52 +0900

    とりあえずindexページを表示させることが完了。これ、コマンドを使えば一発なんだろうな。。。。

--- rails-demo_210923/config/routes.rb
+++ rails-demo_210923/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # ルートアクセスが来た際のルーティングを定義。BoardControllerクラスのindexメソッドを実行するよう定義している。
+  root "boards#index"
 end
