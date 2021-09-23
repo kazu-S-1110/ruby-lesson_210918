a9f6b1ce800a85bae45d1312f02bd10e70e83791
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-09-23 23:55:22 +0900

    bootstrapの定義を外したらindexにアクセスが成功。う〜ん、、

--- rails-demo_210923/config/environments/development.rb
+++ rails-demo_210923/config/environments/development.rb
@@ -52,5 +52,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.reload_classes_only_on_change = false
+  config.web_console.whitelisted_ips = '0.0.0.0/0' #追記
+
 
 end
